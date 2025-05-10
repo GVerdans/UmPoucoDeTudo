128d79f7284693a633bdcc263817166c5a70b4f8
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-05-10 14:01:20 -0300

    include seletores.js

--- 03-JS/04 - DOM/seletores.js
+++ 03-JS/04 - DOM/seletores.js
@@ -2,5 +2,24 @@
 
 document.getElementById(id); // #
 document.getElementsByClassName(Class) // .
-document.getElementsByName(name) // Esse name é a tag dos HTML
+document.getElementsByName(name) // Esse name é a tag do HTML
 document.getElementsByTagName(Tag) // Esse aqui são as tags do HTML tambem, tipo div, a, p.
+
+elemento.innerHTML // Aqui retorna o conteudo HTML COMPLETO, incluindo texto, tags e tal. Aqui dá pra injetar HTML também.
+elemento.innerText // Aqui só retorna o texto 
+elemento.textContent // Mesma coisa do innerText
+
+// QuerySelector
+
+document.querySelector(); // Posso pegar qualquer coisa. Mas apenas uma.
+document.querySelectorAll('p'); // Pega todos os 'p' do documento.
+
+// atribbute Selector
+
+document.querySelector('[name]'); // todos os elementos que contem o atributo NAME
+document.querySelectorAll('p[name]'); // todos os elementos P que contem atributo NAME
+document.querySelectorAll('[name = "texto"]'); // todos os elementos que contem atributo NAME cujo value seja texto.
+document.querySelectorAll('div, p, h1'); // Dá pra agrupar elementos.
+
+
+// Em resumo, todos os seletores CSS se aplicam aos querySelector.
