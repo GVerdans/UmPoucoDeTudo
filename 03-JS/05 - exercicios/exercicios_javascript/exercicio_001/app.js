b2396847f643e332204bc9a358c536d5264c5e3b
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-01 00:30:42 -0300

    Ex01 JS done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_001/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_001/app.js
@@ -8,3 +8,21 @@ Enunciado: CONTADOR
     um dos botões, o utilizador irá aumentar ou diminuir o valor em uma unidade.
 
 ---------------------------------------------------------------------------- */
+
+let contador = 0;
+
+// function attScreen(){
+//     document.querySelector("#valor").innerHTML = contador;
+// }
+
+const btnIncrementa = document.querySelector("#btn_incremento").addEventListener("click", () => {
+    document.querySelector("#valor").innerHTML = ++contador;
+    // attScreen();
+})
+
+const btnDecrementa = document.querySelector("#btn_decremento").addEventListener("click", () => {
+    document.querySelector("#valor").textContent = --contador;
+    // attScreen();
+})
+
+
