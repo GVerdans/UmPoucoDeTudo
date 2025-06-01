5798cf9985cc015a9330341761cbc5857d04a08d
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-01 00:57:05 -0300

    exercices 01 to 03 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_002/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_002/app.js
@@ -8,3 +8,19 @@ Enunciado: CONTADOR COM INTERVALO DE VALORES
     O valor nunca poderá ser inferior a -10 ou superior a 10.
 
 ---------------------------------------------------------------------------- */
+
+let contador = 0;
+
+const btnIncremento = document.querySelector("#btn_incremento");
+const btnDecremento = document.querySelector("#btn_decremento");
+const contadorTela = document.querySelector("#valor");
+
+btnIncremento.addEventListener("click", () => {
+    if(contador == 10) return;
+    document.querySelector("#valor").textContent = ++contador;
+})
+
+btnDecremento.addEventListener("click", () => {
+    if(contador == -10) return;
+    document.querySelector("#valor").textContent = --contador;
+})
--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_003/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_003/app.js
@@ -10,3 +10,23 @@ Enunciado: CONTADOR COM CORES PARA NEGATIVOS, ZERO E POSITIVOS
     No caso do valor ser positivo o texto deve ser verde puro.
 
 ---------------------------------------------------------------------------- */
+
+let contador = 0;
+const contadorTela = document.querySelector("#valor");
+
+document.querySelector("#btn_incremento").addEventListener("click", () => {
+  document.querySelector("#valor").textContent = ++contador;
+  setarValor();
+});
+
+document.querySelector("#btn_decremento").addEventListener("click", () => {
+  document.querySelector("#valor").textContent = --contador;
+  setarValor();
+});
+
+function setarValor() {
+  contadorTela.innerHTML = contador;
+  if (contador == 0) contadorTela.style.color = "white";
+  else if (contador > 0) contadorTela.style.color = "green";
+  else contadorTela.style.color = "red";
+}
