b240839a26e796422af5611eb1c9c6d9145fc54a
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-02 17:21:31 -0300

    ex 007 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_006/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_006/app.js
@@ -9,3 +9,22 @@ Enunciado:
 
     NOTA: quando uma área é apresentada, as outras devem ficar escondidas
 ---------------------------------------------------------------------------- */
+
+function hideAll(){
+document.querySelectorAll("[id^='info']").forEach((el) => { el.classList.add("d-none")}
+)}
+
+function showAll(id){
+    document.querySelector(`#info${id}`).classList.remove("d-none");
+}
+
+document.querySelectorAll("[id^='tab']").forEach((btn) => {
+    btn.addEventListener("click", (btn) => {
+        hideAll();
+        showAll(btn.target.id.substring(3))
+    })
+})
+
+hideAll();
+showAll(1);
+
