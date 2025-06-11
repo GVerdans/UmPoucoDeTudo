5258c24155abd5c5b0a8b6f5ff47a2dfbb063e1c
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-11 10:34:00 -0300

    ex 009 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_009/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_009/app.js
@@ -8,3 +8,26 @@ Enunciado:
 
     NOTA: Deves definir os valores do range.
 ---------------------------------------------------------------------------- */
+
+const range = document.querySelector('#range');
+const texto = document.querySelector('h3');
+
+range.setAttribute('min', 0);
+range.setAttribute('max', 100);
+range.setAttribute('step', 0.1);
+
+let rangeValue = 0;
+
+
+// range.addEventListener('input', (e) => {
+//     rangeValue = e.target.value;
+//     const aplha = 1 - (rangeValue/100);
+//     texto.style.color = `rgba(255, 255, 255, ${aplha})`;
+// });
+
+
+// DAVA PRA FAZER ASSIM TAMBÉM
+
+range.addEventListener('input', (e) => {
+    texto.style.opacity = 1 - e.target.value / 100; 
+});
