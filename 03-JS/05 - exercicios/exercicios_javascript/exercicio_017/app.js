7c9ac42275d984c41739eb1eb219de331b96ae72
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-11 20:58:46 -0300

    ex 17 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_017/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_017/app.js
@@ -7,3 +7,22 @@ Enunciado:
    
     Não pode ser adicionada uma palavra ou frase repetida.
 ---------------------------------------------------------------------------- */
+
+const inputText = document.querySelector("#text_post");
+let lista = [];
+
+document.querySelector("button").addEventListener("click", () => {
+
+    if (!inputText.value == ""){
+        if (!lista.includes(inputText.value)){
+            let newP = document.createElement("p");
+            document.querySelector("#posts").appendChild(newP);
+            newP.textContent = inputText.value;
+            
+            
+            lista.push(inputText.value); // para fazer as validaçoes !
+            inputText.value = "";
+            inputText.focus();
+        };
+    };
+});
