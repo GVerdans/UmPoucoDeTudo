965d176ba1de349903f74edb50302b99407adaed
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-11 12:34:15 -0300

    ex 011 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_011/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_011/app.js
@@ -7,4 +7,21 @@ Enunciado:
     No clique seguinte, que só pode acontecer numa img que não tenha símbolo,
     vamos apresentar o símbolo 'O'.
 ---------------------------------------------------------------------------- */
+let xJoga = true;
 
+const divs = document.querySelectorAll('[id^=img]');
+
+divs.forEach((div) => {
+    div.addEventListener("click", (e) => {
+
+        if (!e.target.style.backgroundImage){
+            if(xJoga){
+                e.target.style.backgroundImage = "url('x.png')"
+            } else {
+                e.target.style.backgroundImage = "url('o.png')"
+            }
+
+            xJoga = !xJoga;
+        }
+    })
+});
