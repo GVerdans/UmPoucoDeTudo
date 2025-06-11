3e2a1b92581f03c68d59c7994e0ec6a9346492b9
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-11 14:45:45 -0300

    ex 012 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_012/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_012/app.js
@@ -5,3 +5,15 @@ Enunciado:
     Ao clicar no botão "Adicionar", se o input de texto não estiver vazio, adicionar
     um parágrafo com esse texto por baixo do botão.
 ---------------------------------------------------------------------------- */
+
+const inputText = document.querySelector("#text_post");
+
+document.querySelector(".btn").addEventListener("click", () => {
+    // createElement("ul", )
+    if (inputText.value == ""){
+        return;
+    } else {
+        document.querySelector("#posts").innerHTML += `<p>${inputText.value}</p>`;
+    }
+    inputText.value = "" ;
+});
