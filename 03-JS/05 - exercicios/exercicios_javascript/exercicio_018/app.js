bdfdc54b9f53004d6235432d9d81e8acb26e488d
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-11 21:42:12 -0300

    ex 018 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_018/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_018/app.js
@@ -11,3 +11,29 @@ Enunciado:
     Se clicar em "Eliminar" apenas poderá ser removida a palavra ou frase
     correspondente.
 ---------------------------------------------------------------------------- */
+
+const inputText = document.querySelector("#text_post");
+let tasks = [];
+
+document.querySelector("button").addEventListener("click", () => {
+    if(inputText.value !== "") {
+        let newP = document.createElement("p");
+        
+        let btnDelete = document.createElement("span");
+        btnDelete.style.cursor = "pointer";
+        btnDelete.classList.add("text-danger");
+        btnDelete.textContent = "Delete";
+        
+        newP.appendChild(btnDelete);
+        newP.append(` => ${inputText.value}`);
+        document.querySelector("#posts").appendChild(newP);
+
+        btnDelete.addEventListener("click", () => {
+            btnDelete.parentNode.remove();
+        })
+
+        inputText.focus();
+        inputText.value = "";
+
+    }
+})
