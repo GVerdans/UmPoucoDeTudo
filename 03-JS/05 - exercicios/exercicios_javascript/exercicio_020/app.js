aecfcb9fce74619b475f21c7ba57288c57058655
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-12 23:44:32 -0300

    ex 020 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_020/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_020/app.js
@@ -7,3 +7,26 @@ Enunciado:
     deve ficar amarela, indicando que o input está ativo. 
     Os inativos devem ficar a branco.
 ---------------------------------------------------------------------------- */
+
+// Esse eu fiz
+
+document.querySelectorAll(".form-control").forEach((input) => {
+    input.addEventListener("focus", (evt) => {
+        document.querySelectorAll(".form-control").forEach((element) => {
+            element.style.background = "white"
+            evt.target.style.background = "yellow";
+        })
+    })
+})
+
+// Esse é o Gabarito '-'
+
+// document.querySelectorAll(".form-control").forEach(e => {
+//     e.addEventListener("focus", () => {
+//         e.style.background = "yellow"
+//     })
+
+//     e.addEventListener("blur", () => {
+//         e.style.background = "white"
+//     })
+// })
