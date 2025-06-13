b7b8b511d9c42437865e0c087148b9a7c7fc872a
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-13 16:52:41 -0300

    ex022 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_022/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_022/app.js
@@ -7,3 +7,22 @@ Enunciado:
     Quando é selecionada a opção 3 no select, as checkboxes devem ficar ativas.
     Qualquer outra opção, devem desativar as checkboxes.
 ---------------------------------------------------------------------------- */
+
+const select = document.querySelector("select");
+
+// document.querySelectorAll("input[type='checkbox']").forEach(cb => {
+//     cb.disabled = true;
+
+select.addEventListener("change", () => {
+
+    if (select.value === "3") {
+        document.querySelectorAll("input[type='checkbox']").forEach(cb => {
+            cb.disabled = false;
+        })
+    } else {
+        document.querySelectorAll("input[type='checkbox']").forEach(cb => {
+            cb.disabled = true;
+        })
+    }
+
+})
