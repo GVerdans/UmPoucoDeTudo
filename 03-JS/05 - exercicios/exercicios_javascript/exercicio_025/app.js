e490801ee404952f45c13050cdd128238b041eca
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-13 21:50:11 -0300

    ex 025 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_025/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_025/app.js
@@ -7,3 +7,13 @@ Enunciado:
     no div cujo id = resultados.
 ---------------------------------------------------------------------------- */
 
+const inputNum = document.querySelector("#text_numero");
+inputNum.setAttribute("min", 1);
+inputNum.setAttribute("max", 20);
+
+inputNum.addEventListener("change", () => {
+    document.querySelector("#resultados").innerHTML = "";
+    for ( let i = 0; i < 11; i++){
+        document.querySelector("#resultados").innerHTML += `${i} x ${inputNum.value} = ${inputNum.value * i} <br>`;
+    }
+})
