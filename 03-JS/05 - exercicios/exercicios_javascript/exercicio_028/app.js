c354ad25eba072fce850bfa6ef66bca44be2ea32
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-06-15 12:46:36 -0300

    ex 028 done

--- 03-JS/05 - exercicios/exercicios_javascript/exercicio_028/app.js
+++ 03-JS/05 - exercicios/exercicios_javascript/exercicio_028/app.js
@@ -6,3 +6,11 @@ Enunciado:
     Esses números devem ser apresentados cada um em sua própria linha dentro do elemento
     cujo id é igual a "numeros".
 ---------------------------------------------------------------------------- */
+ const saida = document.querySelector("#numeros");
+ 
+ document.querySelector("button").addEventListener("click", (e) => {
+     saida.innerHTML = "";
+    for(let i = 0; i < 20; i++){
+        saida.innerHTML += `${Math.floor(Math.random() * 1000) + 1}<br>`;
+    }
+ })
